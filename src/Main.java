af3f1d6b9da347af247a17ecf7df9e9e8096a512
dongbin1999 <dongbin1999@inu.ac.kr>
2020-06-05 21:52:37 +0900

    Main.java modified

--- src/Main.java
+++ src/Main.java
@@ -5,13 +5,14 @@ public class Main {
         int[] items = new int[] {7, 5, 6, 4, 2, 3, 7, 5};
         ArrayList<Bin> arr = new ArrayList<>();
 
+        //이 부분만 수정하면 모든 Fit을 실행할 수 있다.
         Fit f = new NextFit();
         for (int i = 0; i < items.length; i++) {
             f.Fit(arr, new Item(items[i]));
         }
 
         for (int i = 0; i < arr.size(); i++) {
-            System.out.println(arr.get(i));
+            System.out.printlgn(arr.get(i));
         }
 
         System.out.println(arr.size());
